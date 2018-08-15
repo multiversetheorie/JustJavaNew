96c5095bb005848f098dbd0e6e6e90b8ab1027b4
Ádám Kovács <35578457+multiversetheorie@users.noreply.github.com>
2018-08-15 23:02:27 +0200

    Added calculatePrice method. (Incomplete.)

--- app/src/main/java/com/example/android/justjavanew/MainActivity.java
+++ app/src/main/java/com/example/android/justjavanew/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
@@ -53,12 +53,23 @@ public class MainActivity extends AppCompatActivity {
         String priceMessage = "Total: " + price + " HUF";
         priceMessage = priceMessage + "\nThank you!";
         displayMessage(priceMessage);
+
+        calculatePrice();
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @param quantity is the number of cups of coffee ordered
+     */
+    private void calculatePrice() {
+        int price = quantity * 5;
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
